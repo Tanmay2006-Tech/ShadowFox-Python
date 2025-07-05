c257a30cf54157ff309f45e10dfb39d333db8a83
Tanmay2006-Tech <tanmaytripathi7525@gmail.com>
2025-07-06 00:52:24 +0530

    Create 1.2.py

---
+++ 1.2.py
@@ -0,0 +1,8 @@
+for = 4        # This will give a SyntaxError because 'for' is a special word in Python
+# Explanation:
+# In Python, there are some words that are reserved for special use (like 'for', 'if', 'while', etc.)
+# These words are called **keywords** and we can't use them as variable names.
+
+# Let's now use a proper variable name instead
+for_variable = 4  # This is allowed because 'for_variable' is not a keyword
+print("Value stored in for_variable is:", for_variable)
