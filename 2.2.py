2a69863ee0e138496a7589f8de3a55be1cf6ef6b
Tanmay2006-Tech <tanmaytripathi7525@gmail.com>
2025-07-06 01:14:05 +0530

    Create 2.2.py

---
+++ 2.2.py
@@ -0,0 +1,9 @@
+radius = 84
+pi = 3.14
+area = pi * radius * radius
+print("Area of the pond is:", area, "square meters")
+
+water_per_meter = 1.4
+total_water = area * water_per_meter
+total_water = int(total_water)
+print("Total water in the pond is:", total_water, "liters")
